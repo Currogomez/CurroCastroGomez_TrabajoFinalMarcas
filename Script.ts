a81625f6d0eb9ee0845f779d17808fefe2e037dd
curro <currobachillerato@gmail.com>
2026-05-06 20:15:02 +0200

    He anadido otro enum "DificultadAcceso" y he hecho la interfaz del recurso principal

--- Script.ts
+++ Script.ts
@@ -4,3 +4,24 @@ enum CategoriaSitio {
     PuntoInteres = "Punto de interes",
     Restaurante = "Restaurante"
 }
+
+enum DificultadAcceso {
+    Baja = "Baja",
+    Media = "Media",
+    Alta = "Alta"
+}
+
+
+// --- INTERFAZ DEL RECURSO PRINCIPAL ---
+interface SitioTuristico {
+    id: number;
+    nombre: string;
+    tipo: CategoriaSitio;
+    vientoIdeal?: string; // (?)Opcional (solo playas)
+    DificultadAcceso: DificultadAcceso;
+    puntuacion: number;
+    esGratis: boolean;
+    descripcion: string;
+    zona: string;
+    aforo: number;
+}
